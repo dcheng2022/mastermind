614affa86bc0ec59154a76707dc5b4ad7179275f
Darin <darinchengg@gmail.com>
2022-06-24 13:22:09 -0500

    Add invalid guess message and correct create_code implicit return

--- mastermind.rb
+++ mastermind.rb
@@ -4,15 +4,15 @@ def create_code
   choices = %w[red blue green orange purple yellow blank]
   pattern = [choices.sample, choices.sample, choices.sample, choices.sample]
   p pattern
+  pattern
 end
 
 def guess_getter
   choices = %w[red blue green orange purple yellow blank]
+  puts 'Enter a guess.'
   loop do
-    puts 'Enter a guess.'
     guess_array = gets.chomp.split
     return guess_array if guess_array.all? { |guess| choices.include?(guess) } && guess_array.length == 4
+    puts 'Invalid guess! Please try again.'
   end
 end
-
-guess_getter
