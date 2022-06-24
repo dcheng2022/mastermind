a30e0201ca27a2353d0348aaf7ad6ad8df8b49e5
Darin <darinchengg@gmail.com>
2022-06-24 12:31:37 -0500

    Add user input validation

--- mastermind.rb
+++ mastermind.rb
@@ -5,3 +5,14 @@ def create_code
   pattern = [choices.sample, choices.sample, choices.sample, choices.sample]
   p pattern
 end
+
+def guess_getter
+  choices = %w[red blue green orange purple yellow blank]
+  loop do
+    puts 'Enter a guess.'
+    guess_array = gets.chomp.split
+    return guess_array if guess_array.all? { |guess| choices.include?(guess) } && guess_array.length == 4
+  end
+end
+
+guess_getter
