ce7e9b5335b92438c2468dad27e2f4fbfb78511f
Darin <darinchengg@gmail.com>
2022-06-27 12:09:00 -0500

    Complete rudimentary guess check for exact matches and color-only matches

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,6 @@
 require 'pry-byebug'
 
-def create_code
+def code_creator
   choices = %w[red blue green orange purple yellow blank]
   pattern = [choices.sample, choices.sample, choices.sample, choices.sample]
   p pattern
@@ -20,10 +20,28 @@ end
 
 def guess_checker(guess_array, pattern)
   feedback = { color: 0, color_and_pos: 0 }
+  pattern_copy = [].concat(pattern)
+  matched_indices = []
   guess_array.each_with_index do |guess, idx|
-    feedback[:color_and_pos] += 1 if guess == pattern[idx]
+    if guess == pattern_copy[idx]
+      matched_indices << idx
+      feedback[:color_and_pos] += 1
+    end
+  end
+  matched_indices.each do |idx|
+    guess_array[idx] = nil
+    pattern_copy[idx] = nil
+  end
+  guess_array.compact!
+  pattern_copy.compact!
+  #binding.pry
+  guess_array.uniq.each do |guess|
+    pattern_color_amount = pattern_copy.reduce(0) { |sum, color| color == guess ? sum += 1 : sum }
+    guess_color_amount = guess_array.reduce(0) { |sum, color| color == guess ? sum += 1 : sum }
+    feedback[:color] += pattern_color_amount >= guess_color_amount ? guess_color_amount : pattern_color_amount
   end
   puts "Correct color and position: #{feedback[:color_and_pos]}"
+  puts "Correct color only: #{feedback[:color]}"
 end
 
-guess_checker(guess_getter, create_code)
+guess_checker(guess_getter, code_creator)
